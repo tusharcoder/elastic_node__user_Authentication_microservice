e069d4ab83244428a8a21e1a3f0dd00e8661ac64
tusharcoder <tamyworld@gmail.com>
2022-02-13 12:23:25 +0530

    add the function for curl post

--- utils.js
+++ utils.js
@@ -1,8 +1,35 @@
-const moment= require('moment')
+const {Curl} = require('node-libcurl');
+const moment= require('moment');
 
 const utils = class{
     static current_timestamp = function(){
         return moment(Date.now()).format('YYYY-MM-DDTHH:MM:SS')
+    }
+
+    static curl__post = function(endpoint, post_data){
+        
+    const curlTest = new Curl()
+    const terminate = curlTest.close.bind(curlTest);
+    curlTest.setOpt(Curl.option.URL,endpoint);
+    curlTest.setOpt(Curl.option.HTTPHEADER,['Content-Type: application/json']);
+    curlTest.setOpt(Curl.option.POST, true);
+    curlTest.setOpt(
+        Curl.option.POSTFIELDS,
+        JSON.stringify(post_data)
+    );
+    curlTest.on("end", function (statusCode, data, headers) {
+        console.info("Status code " + statusCode);
+        console.info("***");
+        console.info("Our response: " + data);
+        console.info("***");
+        console.info("Length: " + data.length);
+        console.info("***");
+        console.info("Total time taken: " + this.getInfo("TOTAL_TIME"));
+    
+        this.close();
+    });
+    curlTest.on("error", terminate);
+    curlTest.perform();
     }
 }
 
