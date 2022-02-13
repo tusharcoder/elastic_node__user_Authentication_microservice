f2418c1d7f826fc6a7c2e76809528a9e5bc83f11
tusharcoder <tamyworld@gmail.com>
2022-02-13 09:54:05 +0530

    debug the created at and updated at

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+// const dbconnect = require('./dbconnect');
+const utils = require('./utils');
 
+const querystring = require('querystring');
+const {Curl} = require('node-libcurl');
 let port = process.env.PORT || 3000;
 
 app.use('/api/v1', routes);
@@ -16,3 +20,48 @@ app.get('/foo/',(req, res)=>{
     })
 });
 
+app.post('/register/',(req, res)=>{
+    // registration_data = {
+    //     'first_name':req.body.first_name,
+    //     'last_name':req.body.last_name,
+    //     'password':req.body.password,
+    //     'email':req.body.email,
+    //     'username':req.body.username,
+    //     created_at: utils.current_timestamp,
+    //     updated_at: utils.current_timestamp
+    // }
+    registration_data = {
+        'first_name':'Tushar1',
+        'last_name':'Agarwal1',
+        'password':'123456',
+        'email':'tushar.agarwal1@somewhere.com',
+        'username':'tusharagarwal1',
+        'created_at': utils.current_timestamp(),
+        'updated_at': utils.current_timestamp()
+    }
+    const curlTest = new Curl()
+    const terminate = curlTest.close.bind(curlTest);
+    curlTest.setOpt(Curl.option.URL,'http://localhost:9200/user-authentication/_doc/');
+    curlTest.setOpt(Curl.option.HTTPHEADER,['Content-Type: application/json']);
+    curlTest.setOpt(Curl.option.POST, true);
+    curlTest.setOpt(
+        Curl.option.POSTFIELDS,
+        JSON.stringify(registration_data)
+    );
+    curlTest.on("end", function (statusCode, data, headers) {
+        console.info("Status code " + statusCode);
+        console.info("***");
+        console.info("Our response: " + data);
+        console.info("***");
+        console.info("Length: " + data.length);
+        console.info("***");
+        console.info("Total time taken: " + this.getInfo("TOTAL_TIME"));
+    
+        this.close();
+    });
+    curlTest.on("error", terminate);
+    curlTest.perform();
+    return res.json({
+        'message':'OK'
+    })
+});
