398b1fc60a6dfe0268bd83e7a086f78495b215db
tusharcoder <tamyworld@gmail.com>
2022-02-13 12:24:16 +0530

    use the function curl post to do the curl request

--- index.js
+++ index.js
@@ -26,28 +26,7 @@ app.post('/register/',(req, res)=>{
         'created_at': utils.current_timestamp(),
         'updated_at': utils.current_timestamp()
     }
-    const curlTest = new Curl()
-    const terminate = curlTest.close.bind(curlTest);
-    curlTest.setOpt(Curl.option.URL,USER_AUTHENTICATION+DOC_ENDPOINT);
-    curlTest.setOpt(Curl.option.HTTPHEADER,['Content-Type: application/json']);
-    curlTest.setOpt(Curl.option.POST, true);
-    curlTest.setOpt(
-        Curl.option.POSTFIELDS,
-        JSON.stringify(registration_data)
-    );
-    curlTest.on("end", function (statusCode, data, headers) {
-        console.info("Status code " + statusCode);
-        console.info("***");
-        console.info("Our response: " + data);
-        console.info("***");
-        console.info("Length: " + data.length);
-        console.info("***");
-        console.info("Total time taken: " + this.getInfo("TOTAL_TIME"));
-    
-        this.close();
-    });
-    curlTest.on("error", terminate);
-    curlTest.perform();
+    utils.curl__post(USER_AUTHENTICATION+DOC_ENDPOINT, registration_data)
     return res.json({
         'message':'OK'
     })
